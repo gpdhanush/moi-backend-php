b41998b8596c340cd63a36526ef6516cb90441b3
GP Dhanush <35776817+gpdhanush@users.noreply.github.com>
2023-06-14 23:09:30 +0530

    delete data

---
+++ deleteCameraRent.php
@@ -0,0 +1,40 @@
+<?php
+// SET HEADER
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Credentials: true");
+header("Content-Type: application/json; charset=UTF-8");
+
+// INCLUDING DATABASE AND MAKING OBJECT
+require "database.php";
+$db_connection = new Database();
+$conn = $db_connection->dbConnection();
+$data = json_decode(file_get_contents("php://input"));
+
+$sql = "DELETE FROM camera_amount WHERE cm_id='$data->id' ";
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+
+//CHECK WHETHER THERE IS ANY POST IN OUR DATABASE
+if($stmt->rowCount() > 0){
+    http_response_code(200);
+    $posts_array = array(
+        "responseType" => 'S',
+        "responseValue" => [
+            "message" => "Data deleted successfully.",
+        ]
+    );
+}
+else{
+    //IF THERE IS NO POST IN OUR DATABASE
+    http_response_code(400);
+    $posts_array = array(
+        "responseType" => 'F',
+        "responseValue" => [
+            "message" => "Data deleted not successfully.",
+        ]
+    );
+}
+echo json_encode($posts_array);
+?>
